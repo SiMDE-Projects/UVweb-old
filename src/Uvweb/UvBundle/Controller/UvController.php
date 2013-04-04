4c9e3aa0482b80abc1787a79db39fd39c4be775a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-04-04 14:42:06 +0200

    update insert comment lines

--- src/Uvweb/UvBundle/Controller/UvController.php
+++ src/Uvweb/UvBundle/Controller/UvController.php
@@ -12,8 +12,15 @@ class UvController extends Controller
 	{
 		$manager = $this->getDoctrine()->getManager();
 
+
+		//hardcoded uv
+
+		$uvRepository = $manager->getRepository("UvwebUvBundle:Uv");
+		$uv = $uvRepository->find(1);
+
+		//to add a hardcoded comment, uncomment those few lines
 /*		$comment = new Comment();
-		$comment->setAuthor("tkeunebr");
+		$comment->setAuthor("amasciul");
 		$comment->setSemester("P13");
 		$comment->setComment("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. ");
 		$comment->setGlobalrate(6);
@@ -21,19 +28,12 @@ class UvController extends Controller
 		$comment->setWorkamount("Moyenne");
 		$comment->setInterest("Très intéressant");
 		$comment->setPedagogy("Nul");
+		$comment->setPassed(true);
+		$comment->setUv($uv);
 
 		// On récupère l'EntityManager
-		$em = $this->getDoctrine()->getManager();
-		$em->persist($comment);
-		$em->flush();*/
-
-
-		//hardcoded uv
-
-		$uvRepository = $manager->getRepository("UvwebUvBundle:Uv");
-		$uv = $uvRepository->find(1);
-
-
+		$manager->persist($comment);
+		$manager->flush();*/
 
 		$commentRepository = $manager->getRepository('UvwebUvBundle:Comment');
 		$comments = $commentRepository->findAll();
