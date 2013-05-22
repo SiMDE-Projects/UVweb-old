3513a177171dc5e31c97f3da9d1f3e64362f4efd
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-22 15:10:42 +0200

    create Home bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Ornicar\GravatarBundle\OrnicarGravatarBundle(),
             new Uvweb\UvBundle\UvwebUvBundle(),
+            new Uvweb\HomeBundle\UvwebHomeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Uvweb/HomeBundle/UvwebHomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Uvweb\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UvwebHomeBundle extends Bundle
+{
+}
