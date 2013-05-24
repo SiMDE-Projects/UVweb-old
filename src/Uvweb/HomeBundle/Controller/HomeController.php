18a96b5622327d9f4e6371d798d3d989d8d832f5
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-24 16:03:14 +0200

    Use dynamic data in newsfeed

--- src/Uvweb/HomeBundle/Controller/HomeController.php
+++ src/Uvweb/HomeBundle/Controller/HomeController.php
@@ -4,10 +4,19 @@ namespace Uvweb\HomeBundle\Controller;
  
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Uvweb\UvBundle\Entity\Comment;
+
 class HomeController extends Controller
 {
 	public function indexAction() {
-		return $this->render('UvwebHomeBundle:Home:index.html.twig');
+		$manager = $this->getDoctrine()->getManager();
+		$commentRepository = $manager->getRepository('UvwebUvBundle:Comment');
+		$comments = $commentRepository->findBy(
+			array('moderated' => true),
+			array('date' => 'desc'),
+			20,
+			0);
+		return $this->render('UvwebHomeBundle:Home:index.html.twig', array('comments' => $comments ));
 	}
 }
 ?>
