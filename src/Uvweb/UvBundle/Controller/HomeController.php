02812aa7ccb1487a144a6c93012750d0f5c45cbf
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-25 18:41:07 +0200

    Use dynamic news
    Had to add an integer id. A date is not a good match for an id, and is
    rejected as such by Doctrine

--- src/Uvweb/UvBundle/Controller/HomeController.php
+++ src/Uvweb/UvBundle/Controller/HomeController.php
@@ -4,19 +4,25 @@ namespace Uvweb\UvBundle\Controller;
  
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Uvweb\UvBundle\Entity\Comment;
+use Uvweb\UvBundle\Entity\News;
 
 class HomeController extends Controller
 {
 	public function indexAction() {
 		$manager = $this->getDoctrine()->getManager();
 		$commentRepository = $manager->getRepository('UvwebUvBundle:Comment');
+        $newsRepository = $manager->getRepository('UvwebUvBundle:News');
+
+
 		$comments = $commentRepository->findBy(
 			array('moderated' => true),
 			array('date' => 'desc'),
 			20,
 			0);
-		return $this->render('UvwebUvBundle:Home:index.html.twig', array('comments' => $comments ));
+
+        $news = $newsRepository->findLastNews();
+
+		return $this->render('UvwebUvBundle:Home:index.html.twig', array('comments' => $comments, 'news' => $news));
 	}
 }
 ?>
---
+++ src/Uvweb/UvBundle/Entity/News.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Uvweb\UvBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News
+ *
+ * @ORM\Table(name="news")
+ * @ORM\Entity(repositoryClass="Uvweb\UvBundle\Entity\NewsRepository")
+ */
+class News
+{
+    public function __construct()
+    {
+        $this->date = new \Datetime();
+    }
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="smallint", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date", nullable=false)
+     */
+    private $date;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=100, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="texte", type="string", nullable=false)
+     */
+    private $text;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_auteur", type="smallint", nullable=false)
+     */
+    private $authorId;
+
+    /**
+     * @param int $authorId
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->authorId = $authorId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+}
