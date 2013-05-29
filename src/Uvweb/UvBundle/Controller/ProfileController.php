b23a9609e3e1f0ae0963e9593254ce5cd84ad9e4
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-29 15:45:33 +0200

    Plug dynamic data to view in profile

--- src/Uvweb/UvBundle/Controller/ProfileController.php
+++ src/Uvweb/UvBundle/Controller/ProfileController.php
@@ -11,9 +11,23 @@ class ProfileController extends BaseController{
             return $redirect;
         }
 
+        $manager = $this->getDoctrine()->getManager();
+        $userRepository = $manager->getRepository("UvwebUvBundle:User");
+        $commentRepository = $manager->getRepository('UvwebUvBundle:Comment');
+
+        $user = $userRepository->find($userid);
+        if ($user == null) throw $this->createNotFoundException("Cet utilisateur n'existe pas ou plus");
+
+        $comments = $commentRepository->findBy(
+            array('author' => $user, 'moderated' => true),
+            array('date' => 'desc'),
+            20,
+            0);
 
         return $this->render("UvwebUvBundle:Profile:profile.html.twig", array(
-            'searchbar' => $this->searchBarForm->createView()
+            'searchbar' => $this->searchBarForm->createView(),
+            'user' => $user,
+            'comments' => $comments
         ));
     }
 }
