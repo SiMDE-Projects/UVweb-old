cde77b03ececf0f1b989c3b02a80b20e4f5f8a69
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-29 11:42:21 +0200

    Plug basic layour for profile

--- src/Uvweb/UvBundle/Controller/ProfileController.php
+++ src/Uvweb/UvBundle/Controller/ProfileController.php
@@ -6,6 +6,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends BaseController{
     public function displayAction($userid) {
-        return new Response("<body></body>");
+        /** those lines allow redirection after submitting search bar form */
+        if ($redirect = $this->initSearchBar()) {
+            return $redirect;
+        }
+
+
+        return $this->render("UvwebUvBundle:Profile:profile.html.twig", array(
+            'searchbar' => $this->searchBarForm->createView()
+        ));
     }
 }
