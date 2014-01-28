27fcf340948d5d143d5a92cfaf3c428d21921b5a
Maxime <final7765@gmail.com>
2014-01-28 16:18:15 +0100

    First semester shown to user is always current semester.

--- src/Uvweb/UvBundle/Form/CommentType.php
+++ src/Uvweb/UvBundle/Form/CommentType.php
@@ -22,17 +22,20 @@ class CommentType extends AbstractType
         $currentMonth = date('m');
         $currentSemester = 'A';
 
-        if($currentMonth > 1 && $currentMonth < 9) //Spring semester
+        if($currentMonth > 2 && $currentMonth < 9) //Spring semester: march to end of august
             $currentSemester = 'P';
 
         $year = date('Y') % 100; //Starting with current year
+
+        if($currentSemester == 'A' && $currentMonth < 3) // If beginning of 2014: current semester is A13, not A14
+            $year = ($year -1 + 100) % 100;
 
         for($i = 0; $i < 5; $i++)
         {
             if($currentSemester == 'A')
             {
                 if($i % 2 == 0 && !empty($semesters)) //2 Semesters were added (we make sure not to do $year-- if array is still empty)
-                    $year--;
+                    $year = ($year -1 + 100) % 100;
 
                 if($i % 2 == 0)
                     $semesters['A' . $year] = 'A' . $year;
@@ -42,7 +45,7 @@ class CommentType extends AbstractType
             else
             {
                 if($i % 2 == 1 && !empty($semesters)) //2 Semesters were added, but starting on spring
-                    $year--;
+                    $year = ($year -1 + 100) % 100;
 
                 if($i % 2 == 1)
                     $semesters['A' . $year] = 'A' . $year;
